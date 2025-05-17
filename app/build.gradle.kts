bfaa6faa64f7f1f1ad8384cdad7555634761ae10
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-05-17 22:08:37 +0530

    Updater: rebrand to AndroidOne
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,10 +31,10 @@ val keystoreProperties = Properties().apply {
 
 android {
     compileSdk = 34
-    namespace = "com.libremobileos.updater"
+    namespace = "com.aosp.updater"
 
     defaultConfig {
-        applicationId = "com.libremobileos.updater"
+        applicationId = "com.aosp.updater"
         minSdk = 30
         targetSdk = 34
         versionCode = 1
